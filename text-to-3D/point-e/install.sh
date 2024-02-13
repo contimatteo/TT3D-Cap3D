de69720f912568c54b328adbb9a45bd539da1e2c
Matteo Conti <contimatteo1505@gmail.com>
2024-02-13 13:16:01 +0100

    WIP

--- text-to-3D/point-e/install.sh
+++ text-to-3D/point-e/install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-pip install -U pip wheel setuptools
-pip install torch torchvision --index-url https://download.pytorch.org/whl/cu118
+
+CACHE_DIR="/media/data2/mconti/TT3D/cache/pip"
+
+
+pip install -U --cache-dir ${CACHE_DIR} pip wheel setuptools
+pip install --cache-dir ${CACHE_DIR} torch torchvision --index-url https://download.pytorch.org/whl/cu118
 # pip install torch --index-url https://download.pytorch.org/whl/cu121
 pip install -e .
-pip install pyyaml ipywidgets
+pip install --cache-dir ${CACHE_DIR} pyyaml ipywidgets
